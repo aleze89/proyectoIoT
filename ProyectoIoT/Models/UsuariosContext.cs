e1576b79b57b0fe2d81d2cc037ec92d0106d84af
aleze89 <68794504+aleze89@users.noreply.github.com>
2020-11-10 23:23:48 -0300

    problemas con controlador ocasionados por el contexto(solucionado),hay q introducir los cambios en la migracion

--- ProyectoIoT/Models/UsuariosContext.cs
+++ ProyectoIoT/Models/UsuariosContext.cs
@@ -7,23 +7,10 @@ namespace ProyectoIoT.Models
 {
     public class UsuariosContext : DbContext
     {
-        private DbSet<Usuario> usuarios;
-
         public UsuariosContext(DbContextOptions<UsuariosContext> options)
-: base(options)
-        { }
+            : base(options)
+            { }
 
-        public object Usuarios { get; internal set; }
-
-        public DbSet<Usuario> GetUsuarios()
-        {
-            return usuarios;
-        }
-
-        public void SetUsuarios(DbSet<Usuario> value)
-        {
-            usuarios = value;
-        }
+            public DbSet<Usuario> Usuarios { get; set; }
     }
-
 }
