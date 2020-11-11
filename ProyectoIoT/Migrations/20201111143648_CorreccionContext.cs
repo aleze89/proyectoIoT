648df37aa0bc358743a2e4edb38f67ed2158ee94
aleze89 <68794504+aleze89@users.noreply.github.com>
2020-11-11 11:41:08 -0300

    base de datos creada

---
+++ ProyectoIoT/Migrations/20201111143648_CorreccionContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProyectoIoT.Migrations
+{
+    public partial class CorreccionContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    ID = table.Column<long>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NombreUsuario = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Clave = table.Column<string>(maxLength: 6, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
